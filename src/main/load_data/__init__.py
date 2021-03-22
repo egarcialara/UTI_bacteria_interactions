8ca7a9882e936ef8fbeb2cd9c1a41456fae74f4f
egarcialara <e.oohla@hotmail.com>
2021-03-22 22:38:29 +0100

    load data scripts

---
+++ src/main/load_data/__init__.py
@@ -0,0 +1,4 @@
+from .load_KOs_data import create_KOs_binary_matrix#matrix_abundances_per_pathway#, creating_matrix_abundances
+from .util.KEGG_pathways import get_list_ko
+from .util.strains_names import change_names
+from .load_growth_data import mat_to_pandas
