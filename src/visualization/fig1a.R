15a36b3dbd3d1ebd7b629fd8636b39c5c564390d
Elena <35764654+egarcialara@users.noreply.github.com>
2021-05-02 14:39:06 +0200

    update fig1a

--- src/visualization/fig1a.R
+++ src/visualization/fig1a.R
@@ -1,143 +1,102 @@
-# Required packages
-# if (!requireNamespace("BiocManager", quietly = TRUE))
-#   install.packages("BiocManager")
-# BiocManager::install("ComplexHeatmap")
-# BiocManager::install("ClassDiscovery")
-
 library(ComplexHeatmap)
-library(ClassDiscovery)
 library(dplyr)
-library(tibble)
-library("RColorBrewer")
+library(RColorBrewer)
 library(tidyr)
-library(stringr)
 
-# Global variables
-source_dir = "~/Documents/GitHub/UTI_bacteria_interactions/src/visualization/util/fig2/temp/KO_binary_matrices/"
-source_files = list.files(source_dir, pattern=".csv$")
-saving_dir = "~/Documents/GitHub/UTI_bacteria_interactions/figures/KO_binary/"
-saving_format = ".pdf" # if different, change function at the end of the script
-
-
-pathways_nope <- c("Carbapenem biosynthesis", "Fatty acid elongation", "Penicillin and cephalosporin biosynthesis",
-                   "Primary bile acid biosynthesis", "Steroid biosynthesis", "Steroid hormone biosynthesis",
-                   "Synthesis and degradation of ketone bodies", "Tetracycline biosynthesis"
-)
-
-# ----------------------------------------------------------------------------
-# Main function
-# Call it at the end of the script
 make_KO_binary_plots_colourized <- function(file, source_dir){
-
-# Read files
-
+  
+  # Data
   df = read.csv(paste(source_dir,file,sep=""), header=TRUE, row.names=1)
   pathway_name = substr(file, 1, (nchar(file)-4))
-  print(pathway_name)
   
-  if (pathway_name %in% pathways_nope){
-    print("Pass. Error with pathway.")
-    next
-  }
   
-  if (sum(df!=0)==0) {
-    print("Pass. No KOs present.")
-    next
-  }
+  # Groups 
+  df2 <- df %>%
+    mutate(labels = rownames(df)) %>%
+    separate(labels, c("Name"), sep="[.]", remove=TRUE, extra="drop")
   
-  # df <- df %>% mutate(original_index=2:(n()+1))
   
-  # Split groups and make different values for 'KO present' in each
-  group1 = df[str_detect(rownames(df), "KECS"), ]
-  group2 = df[str_detect(rownames(df), "Ecoli"), ]
-  group3 = df[str_detect(rownames(df), "St"), ]
-  group4 = df[str_detect(rownames(df), "Ps"), ]
-  group5 = df[str_detect(rownames(df), "Ent"),]
-  group6 = df[str_detect(rownames(df), "Pm"), ]
+  # Order
+  h_order <- Heatmap(as.matrix(df), name = "mat", right_annotation = rowAnnotation(foo = anno),
+                row_split = df2$Name)
+  order_rows <- row_order(h_order)
+  order_cols <- column_order(h_order)
+  
+  
+  # Colours
+  # (each value, one number. Zero remains 0)
+  df3 <- df2 %>%
+    mutate(across(where(is.numeric), 
+        ~ifelse(Name=="Ent", .*1,
+        ifelse(Name=="St", .*2,
+        ifelse(Name=="Ps", .*3,
+        ifelse(Name=="Pm", .*4,
+        ifelse(Name=="KECS", .*5,
+        ifelse(Name=="Ecoli", .*6, NA_real_)
+        )))))))%>%
+    mutate_if(is.numeric, as.character) %>%
+    select(-Name)
+  
+  col <- c(brewer.pal(n = 6, name = "Dark2"))
+  col <- c("#FFFFFF", col) # add white for 0
+  
+  # Annotations
+  text_list = list(
+    text1 = "Ecoli", text2="Ent", text3="KECS",
+    text4="Pm", text5="Ps", text6="St")
+  ha = rowAnnotation(
+    empty = anno_empty(border=FALSE),
+    foo = anno_empty(border = FALSE, 
+          width = max_text_width(unlist(text_list)) + unit(4, "mm")))
 
-  group1[group1==1]="KECS"
-  group2[group2==1]="Ecoli"
-  group3[group3==1]="St"
-  group4[group4==1]="Ps"
-  group5[group5==1]="Ent"
-  group6[group6==1]="Pm"
   
-  
-  # Put groups again
-  df2 = rbind(group1, group2, group3, group4, group5, group6)
-  # df2 <- df2 %>%
-    # mutate(grouped_index = 1:n()) %>%
-    # arrange(as.integer(original_index)) %>%
-    # select(-original_index)
-  # order
-  hm_order <- ComplexHeatmap::Heatmap(as.matrix(df), cluster_rows=TRUE, cluster_columns = TRUE)
-  # ro_1 <- ComplexHeatmap::row_order(hm_order)
-  co_1 <- ComplexHeatmap::column_order(hm_order)
-  # Order 2
-  order <- c()
-  n <- 0
-  list_groups <- list(group1, group2, group3, group4, group5, group6)
-  for(group in list_groups){
-    group[group!=0]=1
-    hm_temp <- ComplexHeatmap::Heatmap(as.matrix(group%>%select), cluster_rows=TRUE, cluster_columns = TRUE)
-    ro_temp <- ComplexHeatmap::row_order(hm_temp)
-    order <- c(order, (ro_temp+n))
-    n <- n + length(ro_temp)
-  }
-  
-  # Use RColorBrewer color palette names
-  col <- c(brewer.pal(n = 7, name = "Dark2"))
-
   # Heatmap
-  df2[df2==0] = NA # to make background of heatmap white :)
-  hm_main <- ComplexHeatmap::Heatmap(as.matrix(df2),
-          cluster_rows = TRUE, cluster_columns = TRUE,
-          column_order = co_1,
-          row_order = order, # try put also order by groups
-          na_col = "white", # easy way to colour all 0's in white (see line 58)
-          col=col,
-          name = pathway_name,
+  hh <- Heatmap(as.matrix(df3), name = "mat", 
+          # right_annotation = ha, # !!! un-comment if annotations are added!!!
+          row_split = df2$Name,
+          row_order = unlist(order_rows),
+          column_order = order_cols,
           show_row_names = FALSE, show_column_names = FALSE,
-          column_title = "KOs", row_title = "Genera",
+          show_row_dend = FALSE, show_column_dend = FALSE,
+          col=col,
+          column_title = "KOs",
           column_title_side='bottom',
-          width = 5,
-          show_heatmap_legend=FALSE
-  )
+          row_title="Strains",
+          show_heatmap_legend=FALSE,
+          column_title_gp = gpar(fontsize = 11),
+          row_title_gp = gpar(fontsize = 11)
+          )
   
-  df_names <- data.frame(groups=as.factor(rownames(df2)), order=order) %>%
-    arrange(order) %>%
-    tidyr::separate(col='groups', into=c('genera', 'extra'))%>%
-    select(genera)
-  hm_names <- ComplexHeatmap::Heatmap(as.matrix(df_names),
-                            cluster_rows=FALSE,
-                            show_column_names = FALSE, 
-                            col=col[-length(col)],
-                            width = 0.125,
-                            show_heatmap_legend=FALSE
-                            )
-
-
-  hm <- hm_main + hm_names
-  return(hm)
-  # draw(hm, column_title = pathway_name)
+  # Annotations (part 2, add to image created)
+  # (If you want them, un-comment the right_annotation above !!! )
+  add_annotations <- function(){
+  col2 <- c(brewer.pal(n = 6, name = "Dark2")) # remove white
+  col2 = col2[c(6,1,5,4,3,2)]
+  for(i in 1:6) {
+    decorate_annotation("foo", slice = i, {
+      grid.rect(x = 0, width = unit(1, "mm"), 
+                gp = gpar(fill = col2[i], col = NA),
+                just = "left")
+      grid.text(paste(text_list[[i]], collapse = "\n"), 
+                x = unit(2, "mm"), just = "left",
+                gp=gpar(fontsize=9))
+    })}
+  }
   
   
-
+  return(list(
+    hh = hh,
+    function_add_annotations= add_annotations
+  ))
+   
 }
 
-file = "all.csv"
-hm_temp <- make_KO_binary_plots_colourized(file, source_dir)
-# Save
-# dir.create(file.path(saving_dir), showWarnings = FALSE)
-# pdf(paste(saving_dir, pathway_name, saving_format, sep=""))
-# draw(hm_temp, column_title = pathway_name)
-# dev.off()
 
-# for (file in source_files){
-#   hm_temp <- make_KO_binary_plots_colourized()
-#   # Save
-#   dir.create(file.path(saving_dir), showWarnings = FALSE)
-#   pdf(paste(saving_dir, pathway_name, saving_format, sep=""))
-#   draw(hm_temp, column_title = pathway_name)
-#   dev.off()
-# }
+# # To run:
+# file_1a = "all.csv"
+# source_1a = "~/Documents/GitHub/UTI_bacteria_interactions/src/visualization/util/fig2/temp/KO_binary_matrices/"
+# return_1a <- make_KO_binary_plots_colourized(file_1a, source_1a)
+# hm_1a <- return_1a$hh
+# func_1a <- return_1a$function_add_annotations
+
+
